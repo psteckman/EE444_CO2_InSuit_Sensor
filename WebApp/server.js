6026b693812ca137458168a94601a69a735260d0
Ryker Dial <ryker.dial@gmail.com>
2016-03-29 23:39:15 -0800

    Changed baud rate to 57600, tested with 5 dummy sensors. Now has signed sensor data

--- WebApp/server.js
+++ WebApp/server.js
@@ -34,7 +34,7 @@ io.sockets.on('connection', function (socket) {
     // Start transmitting data to the client
     var interval = setInterval(function() {
         socket.emit('Sensor Data', {data: data_controller.sensor_data});
-        console.log(data_controller.sensor_data);
+        //console.log(data_controller.sensor_data);
     }, data_controller.update_interval);
     
     // ***** Server commands section
@@ -53,7 +53,7 @@ var SerialPort = serialport.SerialPort; // make a local instance
 
 var portName = process.argv[2]; // get port name from the command line
 var sp = new SerialPort(portName, {
-   	baudRate: 9600,
+   	baudRate: 57600,
    	dataBits: 8,
    	parity: 'none',
    	stopBits: 1,
@@ -66,17 +66,18 @@ var data_controller = {
     ID_size: 3, // Size in bytes of sensor data ID used to partition packet received over serial.
     part_size: 5, // Size in bytes of one data partition
     data_idx: 0, // location of parser extracting sensor data from packet received over serial.
-    update_interval: 50, // How often the server sends sensor data to the client.
+    update_interval: 10, // How often the server sends sensor data to the client.
     numsensors: 2, // Number of sensors attached to the system. !!!REMEMBER TO CHANGE THIS TO 0 AFTER TESTING!!!
     sensor_data: {} // Holds sensor data.
 };
-data_controller.data_bytes = data_controller.part_size - data_controller.ID_size; // Number of sensor data bytes per partition
+// Number of sensor data bytes per partition. Must be some power of 2.
+data_controller.data_bytes = data_controller.part_size - data_controller.ID_size;  
 
 function sendToClient(data) {
     data_controller.data_idx = 0;
     //CO2_data = data.substring(3, 8); // CO2 conc. after digital filtering
     //CO2_data = parseInt(CO2_data)*10;
-    for(i=0; i < 3; ++i) {
+    for(i=0; i < 5; ++i) {
         parse_serial_data(data);
     }
 }
@@ -105,6 +106,43 @@ function parse_serial_data(data) {
                 data_controller.sensor_data.FLO_data += 
                     data[data_controller.data_idx+data_controller.ID_size+j]
                     << 8*(data_controller.data_bytes-(j+1));
+            }
+            break;
+        case "ACC":
+            data_controller.sensor_data.ACC_data = 0; // clear old data
+            for(j=0; j < data_controller.data_bytes; ++j) {
+                data_controller.sensor_data.ACC_data += 
+                    data[data_controller.data_idx+data_controller.ID_size+j]
+                    << 8*(data_controller.data_bytes-(j+1));
+                // Convert to signed number.
+                if(data_controller.sensor_data.ACC_data > Math.pow(2,data_controller.data_bytes*8)/2 - 1) {
+                    data_controller.sensor_data.ACC_data = data_controller.sensor_data.ACC_data - Math.pow(2,data_controller.data_bytes*8);
+                }
+            }
+            //data_controller.sensor_data.ACC_data = data_controller.sensor_data.ACC_data;
+            break;
+        case "GYR":
+            data_controller.sensor_data.GYR_data = 0; // clear old data
+            for(j=0; j < data_controller.data_bytes; ++j) {
+                data_controller.sensor_data.GYR_data += 
+                    data[data_controller.data_idx+data_controller.ID_size+j]
+                    << 8*(data_controller.data_bytes-(j+1));
+                // Convert to signed number.
+                if(data_controller.sensor_data.GYR_data > Math.pow(2,data_controller.data_bytes*8)/2 - 1) {
+                    data_controller.sensor_data.GYR_data = data_controller.sensor_data.GYR_data - Math.pow(2,data_controller.data_bytes*8);
+                }            
+            }
+            break;
+        case "MAG":
+            data_controller.sensor_data.MAG_data = 0; // clear old data
+            for(j=0; j < data_controller.data_bytes; ++j) {
+                data_controller.sensor_data.MAG_data += 
+                    data[data_controller.data_idx+data_controller.ID_size+j]
+                    << 8*(data_controller.data_bytes-(j+1));
+                // Convert to signed number.
+                if(data_controller.sensor_data.MAG_data > Math.pow(2,data_controller.data_bytes*8)/2 - 1) {
+                    data_controller.sensor_data.MAG_data = data_controller.sensor_data.MAG_data - Math.pow(2,data_controller.data_bytes*8);
+                }
             }
             break;
     }
