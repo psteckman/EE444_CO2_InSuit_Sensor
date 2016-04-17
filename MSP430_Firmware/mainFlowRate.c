b8cdf9fc979827d24cb90e27adc98df620aa0f78
cjtaylor4 <cjtaylor4@alaska.edu>
2016-04-16 16:58:09 -0800

    Update mainFlowRate.c

--- MSP430_Firmware/mainFlowRate.c
+++ MSP430_Firmware/mainFlowRate.c
@@ -8,11 +8,15 @@ char rtc_flag=0;
 char DiffPress;
 
 //local vars for TI code
-/*
+
 unsigned char *PRxData;                     // Pointer to RX data
-unsigned char RXByteCtr;
+unsigned char RXByteCtr = 2;
+unsigned char TXByteCtr = 3;                // Counter for Tx bytes (address write, measurement command, address read)
+unsigned char sensorMeasCmd = 0xf1;         // Command for measuring from the Sensiron Diff. Pressure sensor. 
+unsigned char writeAddress = 0x80;
+unsigned char readAddress = 0x81; 
 volatile unsigned char RxBuffer[128];       // Allocate 128 byte of RAM
-*/
+
 
 void main(void)
 {
@@ -22,14 +26,14 @@ void main(void)
   P3SEL |= BIT7; //Set 3.7 to I2C data function
   P5SEL |= BIT4; // Set 5.4 to I2C clock function
 
-  P3DIR &= ~BIT7; //Set I2C data pin as input for data from sensor
-  P5DIR |= BIT4; //Set MSP Clock as output for sensor to use.
+  //P3REN |= BIT7; //Enable pull-up resistors for SDA and SCL
+  //P5REN |= BIT4; 
 
   P1DIR |= BIT0 + BIT1; //set LEDs as outputs
   P1OUT &= ~(BIT0 + BIT1); // Turn off LEDs
 
   UCB1I2CSA = 0x81; //I2C address = 0x80 by default. Add 1 to indicate we want to read from the slave. 
- 
+
   UCB1CTL0 |= UCMODE_3 + UCSYNC + UCMST; //Set to I2C mode, synchronous, master mode. 
 
   UCB1CTL1 |= UCSSEL__SMCLK + UCSWRST; //Select SMCLK as source
@@ -37,7 +41,11 @@ void main(void)
   UCB1BR0 = 10;
   UCB1BR1 = 0; 
  
+  UCB1IE |= UCRXIE + UCNACKIE + UCTXIE;
+
   UCB1CTL1 &= ~UCSWRST; //Release reset condition
+
+ 
 
   // Set up RTC registers
   RTCPS0CTL |= RT0PSDIV_7; // divide ACLK by 256
@@ -50,19 +58,26 @@ void main(void)
   RTCPS0CTL &= ~(RT0PSHOLD); 
   RTCPS1CTL &= ~(RT1PSHOLD); 
 
-  //I2C interrupt vectors, USCIAB0TX_VECTOR for both rx and tx. USCIAB0RX_VECTOR used for four state-change vectors in UCB0STAT
   _EINT();
-  
-  for(i = 0; i<6; i++) {
-    switch(bs) {
-      case 0: 
-        break;
-      case 1:
-        break;
-      default:
-         return;      
-    }
+
+
+  while (1)
+  {
+    PRxData = (unsigned char *)RxBuffer;    // Start of RX buffer
+    RXByteCtr = 2;                          // Load RX byte counter
+    while (UCB1CTL1 & UCTXSTP);             // Ensure stop condition got sent
+    UCB1CTL1 |= UCTXSTT;                    // I2C start condition
+
+    __bis_SR_register(LPM0_bits + GIE);     // Enter LPM0, enable interrupts
+                                            // Remain in LPM0 until all data
+                                            // is RX'd
+    __no_operation();                                   
   }
+
+
+  //I2C interrupt vectors, USCIAB0TX_VECTOR for both rx and tx. USCIAB0RX_VECTOR used for four state-change vectors in UCB0STAT
+
+}
 
   //UCSI Interrupt
   // Case 4 is slave did not ACK - set for stop or repeated start. 
@@ -79,7 +94,9 @@ __interrupt void USCI_B1_ISR(void)
   {
   case  0: break;                           // Vector  0: No interrupts
   case  2: break;                           // Vector  2: ALIFG
-  case  4: break;                           // Vector  4: NACKIFG
+  case  4: 
+    UCB1CTL1 |= UCTXSTP;                    // Vector  4: NACKIFG
+    break;                           
   case  6: break;                           // Vector  6: STTIFG
   case  8: break;                           // Vector  8: STPIFG
   case 10:                                  // Vector 10: RXIFG
@@ -115,23 +132,26 @@ __interrupt void USCI_B1_ISR(void)
                                             // is RX'd
     __no_operation();                       // Set breakpoint >>here<< and
   } */
-}
 
-/*#pragma vector=RTC_VECTOR // RTC Interrupt
+
+#pragma vector=RTC_VECTOR // RTC Interrupt
 __interrupt void timer_a (void) {
   switch(RTCIV)
   {
-    case(RTCIV_RT1PSIFG):      
+    case(RTCIV_RT1PSIFG):  
       P1OUT ^= BIT1;
-      //rtc_value++;
-      ///Reset rtc every minute for timed flash saving. 
-      if (rtc_value==30) { //Recall rtc increases by 1 every 2 seconds. Thus 30 for rtc_count corresponds to 1 minute. 
-        rtc_value=0;
-        rtc_flag=1;
-        LPM0_EXIT;
-        }
+      Transmit(); 
       break;
     default:
       return;
   }
-}*/
+}
+
+
+void Transmit(void) {
+  while (UCB1CTL1 & UCTXSTP);       // Ensure stop condition got sent
+  UCB1CTL1 |= UCTXSTT;             // I2C TX, start condition
+  //Transmit read byte 0x80, command bit 0xF1, and write bit 0x81. 
+  _EINT();
+  LPM0; 
+}
